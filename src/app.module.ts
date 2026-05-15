a4b9d1e7ab1d69a843da74b0c1ba3b19b20a6482
Jeaninny Teixeira <jeaninny.teixeira@gmail.com>
2026-05-15 15:55:58 -0300

    :wrench: configuração do TypeORM, variáveis de ambiente e scripts de migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        synchronize: false,
+        entities: [],
+        autoLoadEntities: true,
+        migrations: [__dirname + '/migrations/**/*{.js,.ts}'],
+        migrationsTableName: 'migrations',
+
+      }),
+    })
+  ],
+  controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
